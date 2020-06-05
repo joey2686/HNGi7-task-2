eae6cc511d90d19e69a5ed4cba76b14ad3e8bd50
joey2686 <66218936+joey2686@users.noreply.github.com>
2020-06-05 14:35:53 -0700

    Create josephokite-HNG-01287.js

---
+++ scripts/josephokite-HNG-01287.js
@@ -0,0 +1,6 @@
+const name ="Joseph Okite";
+const ID = "HNG-01287";
+const language = "JavaScript";
+const email = "josephokite@gmail.com";
+const result = "Hello World, this is" + " " + name + " " + "with HNGi7 ID" + " " + ID + " " + "using" + " " + language + " " + "for stage 2 task";
+console.log(result)
